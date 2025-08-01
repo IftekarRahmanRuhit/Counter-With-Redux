79c9f7e28c5069c4014514b3170b5ae900420b1c
Iftekar Rahman Ruhit <iftekarrahmanruhit2003@gmail.com>
2025-08-01 10:38:11 +0600

    initial setup complete

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+
+
+  return (
+<div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold mb-4">Counter with Redux</h1>
+      <button className="btn btn-secondary">Increment</button>
+      <div className="font-bold mt-2 text-2xl">0</div>
+      <button className="btn btn-warning">decrement</button>
+    </div>
+  )
+}
+
+export default App
