d9699ff5770f0f7817c516b5b822dcb7949ca1cd
Iftekar Rahman Ruhit <iftekarrahmanruhit2003@gmail.com>
2025-08-01 12:10:34 +0600

    added hooks for type safety and made counter dynamic

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
-import { useDispatch } from "react-redux";
+
 import { decrement, increment } from "./redux/features/counter/counterSlice";
+import { useAppDispatch, useAppSelector } from "./redux/hook";
 
 function App() {
-  const dispatch = useDispatch();
 
+  // Hook to access the dispatch function from Redux store
+  const dispatch = useAppDispatch();
+
+// Function to handle increment action
   const handleIncrement = () => {
     dispatch(increment());
   };
+
+  // Function to handle decrement action
   const handleDecrement = () => {
     dispatch(decrement());
   };
+
+  // Accessing the count from the Redux store
+  const {count} = useAppSelector((state) => state.counter);
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
@@ -17,7 +26,9 @@ function App() {
       <button onClick={handleIncrement} className="btn btn-secondary">
         Increment
       </button>
-      <div className="font-bold mt-2 text-2xl">0</div>
+      <div className="font-bold mt-2 text-2xl mb-2 ">
+        <button className="btn btn-circle bg-amber-100 text-black text-2xl">{count}</button>
+      </div>
       <button onClick={handleDecrement} className="btn btn-warning">
         decrement
       </button>
